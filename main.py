4426fb000f9c39df1a345f8297ee6086667f41a8
petardaszymek <130040535+petardaszymek@users.noreply.github.com>
2024-09-21 04:00:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from scrap import main as scrap_main
 from clean import clean_data
 from sentiment import analyze_data
 from ingest_curr_vals import fetch_coin_data
+from correlation_analysis import analyze_data
+from charts import chart
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
-
 
 async def run_all_tasks():
     logging.info("Starting Twitter scraping...")
@@ -23,6 +24,11 @@ async def run_all_tasks():
     logging.info("Starting currency data ingestion...")
     fetch_coin_data()
 
+    logging.info("Starting correlation analysis...")
+    analyze_data()
+
+    logging.info("Starting creating charts...")
+    chart()
 
 if __name__ == "__main__":
     asyncio.run(run_all_tasks())
